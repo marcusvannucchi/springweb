19c98e5f682b2672b971bfbfd42a5f891172416c
marcusvannucchi <marcus.vannucchi@etec.sp.gov.br>
2024-05-24 22:07:02 -0300

    first commit

---
+++ aulajavaspring/src/test/java/aulajava/aulajavaspring/AulajavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package aulajava.aulajavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulajavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
